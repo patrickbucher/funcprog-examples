000ef5718545a1b7c53dae277afe0c36034fa74f
Patrick Bucher <patrick.bucher@mailbox.org>
2022-03-28 19:37:21 +0200

    shortened code

--- identity/identity.go
+++ identity/identity.go
@@ -12,35 +12,25 @@ type Function[T any] func(T) T
 
 // Map performs the given Function on the Identity.
 func (i Identity[T]) Map(f Function[T]) Identity[T] {
-	return Identity[T]{
-		Value: f(i.Value),
-	}
+	return Identity[T]{Value: f(i.Value)}
 }
 
 // Compose composes the functions f and g, returning a function f(g(x)).
 func Compose[T any](f, g Function[T]) Function[T] {
-	return func(x T) T {
-		return f(g(x))
-	}
+	return func(x T) T { return f(g(x)) }
 }
 
 func main() {
 	// 1st Functor Law: Identity
-	identity := func(v int) int {
-		return v
-	}
+	identity := func(v int) int { return v }
 
 	a := Identity[int]{3}
 	b := a.Map(identity)
 	fmt.Println(a, b)
 
 	// 2nd Functor Law: Composition
-	twice := func(v int) int {
-		return v * 2
-	}
-	inc := func(v int) int {
-		return v + 1
-	}
+	twice := func(v int) int { return v * 2 }
+	inc := func(v int) int { return v + 1 }
 
 	m := Identity[int]{3}
 	n := m.Map(twice).Map(inc)
